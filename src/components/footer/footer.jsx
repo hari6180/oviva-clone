61f788974c333597d9dc9a4609a2b0e1966f6e52
Hari Jeon <ori080525@gmail.com>
2021-08-24 12:59:48 +0900

    CSS:footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,39 +4,43 @@ import styles from "./footer.module.css";
 const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      <div>
-        <img src="img/foot-logo.png"></img>
-      </div>
-      <section>
-        <div>
-          <h5>Useful Links</h5>
-          <ul>
-            <li>Privacy Policy</li>
-            <li>Terms of Service</li>
-            <li>Work for us</li>
-            <li>FAQs</li>
-            <li>Sitemap</li>
-            <li>Accessibility Statement</li>
-            <li>‘Oviva App’ User manual</li>
-            <li>Oviva Global</li>
-          </ul>
-          <p>All Systems operational</p>
+      <div className={styles.footer__wrap}>
+        <div className={styles.menu__wrap}>
+          <div className={styles.logo__wrap}>
+            <img className={styles.logo} src="img/foot-logo.png" alt="logo" />
+          </div>
+          <div className={styles.menus}>
+            <div className={styles.menu}>
+              <h5 className={styles.title}>Useful Links</h5>
+              <ul className={styles.menu_list}>
+                <li>Privacy Policy</li>
+                <li>Terms of Service</li>
+                <li>Work for us</li>
+                <li>FAQs</li>
+                <li>Sitemap</li>
+                <li>Accessibility Statement</li>
+                <li>‘Oviva App’ User manual</li>
+                <li>Oviva Global</li>
+              </ul>
+              <p>All Systems operational</p>
+            </div>
+            <div className={styles.menu}>
+              <h5 className={styles.title}>Contact</h5>
+              <p className={styles.menu_list}>Tel: +8210 8070 6180</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h5>Contact</h5>
-          <p>Tel: +8210 8070 6180</p>
+        <div className={styles.link__wrap}>
+          <a href="#" className={styles.link}>
+            <img className={styles.link_img} src="img/twitter-icon.png" alt="twitter" />
+          </a>
+          <a href="#" className={styles.link}>
+            <img className={styles.link_img} src="img/email-icon.png" alt="email" />
+          </a>
+          <a href="#" className={styles.link}>
+            <img className={styles.link_img} src="img/foot-top.png" alt="top" />
+          </a>
         </div>
-      </section>
-      <div>
-        <a href="#">
-          <img src="img/twitter-icon.png" alt="twitter" />
-        </a>
-        <a href="#">
-          <img src="img/email-icon.png" alt="email" />
-        </a>
-        <a href="#">
-          <img src="img/foot-top.png" alt="top" />
-        </a>
       </div>
     </footer>
   );
